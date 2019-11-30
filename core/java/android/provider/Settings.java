7d95a93d8bba2d37a8b612d36a8114ad65c6cded
SuperDroidBond <superdroidbond@yahoo.com>
2019-11-30 21:18:53 +0530

    VoLTE Icon - User Selectable VoLTE Icon [1/2]
    Idea Taken from https://github.com/Havoc-OS/android_frameworks_base/commit/61ff6b24a01cf92933e2461403dca88b7e255663
    So Thanks to @SKULSHADY & @neobuddy89 for fixes :)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6320,6 +6320,15 @@ public final class Settings {
         public static final Validator SHOW_VOLTE_ICON_VALIDATOR = BOOLEAN_VALIDATOR;
 
         /**
+         * Whether to show VoLTE icon or not
+         * @hide
+         */
+        public static final String VOLTE_ICON_STYLE = "volte_icon_style";
+
+        /** @hide */
+        public static final Validator VOLTE_ICON_STYLE_VALIDATOR = ANY_INTEGER_VALIDATOR;
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -6679,6 +6688,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(AUTO_BRIGHTNESS_MIN_VALUE);
             PRIVATE_SETTINGS.add(NAVIGATION_HANDLE_WIDTH);
             PRIVATE_SETTINGS.add(SHOW_VOLTE_ICON);
+            PRIVATE_SETTINGS.add(VOLTE_ICON_STYLE);
         }
 
         /**
@@ -6870,6 +6880,7 @@ public final class Settings {
             VALIDATORS.put(AUTO_BRIGHTNESS_MIN_VALUE, AUTO_BRIGHTNESS_MIN_VALUE_VALIDATOR);
             VALIDATORS.put(NAVIGATION_HANDLE_WIDTH, NAVIGATION_HANDLE_WIDTH_VALIDATOR);
             VALIDATORS.put(SHOW_VOLTE_ICON, SHOW_VOLTE_ICON_VALIDATOR);
+            VALIDATORS.put(VOLTE_ICON_STYLE, VOLTE_ICON_STYLE_VALIDATOR);
         }
 
         /**
--- packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
@@ -22,6 +22,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.res.Resources;
 import android.database.ContentObserver;
+import android.graphics.drawable.Drawable;
 import android.net.NetworkCapabilities;
 import android.net.Uri;
 import android.os.Handler;
@@ -114,6 +115,8 @@ public class MobileSignalController extends SignalController<
 
     // Volte Icon
     private boolean mVoLTEicon;
+    // Volte Icon Style
+    private int mVoLTEstyle;
 
     // TODO: Reduce number of vars passed in, if we have the NetworkController, probably don't
     // need listener lists anymore.
@@ -211,6 +214,9 @@ public class MobileSignalController extends SignalController<
             resolver.registerContentObserver(
 	            Settings.System.getUriFor(Settings.System.SHOW_VOLTE_ICON), false,
 		    this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(
+	            Settings.System.getUriFor(Settings.System.VOLTE_ICON_STYLE), false,
+		    this, UserHandle.USER_ALL);
             updateSettings();
         }
 
@@ -244,8 +250,12 @@ public class MobileSignalController extends SignalController<
                 UserHandle.USER_CURRENT) == 1;
 
         mVoLTEicon = Settings.System.getIntForUser(resolver,
-                Settings.System.SHOW_VOLTE_ICON, 0,
+                 Settings.System.SHOW_VOLTE_ICON, 0,
                 UserHandle.USER_CURRENT) == 1;
+
+       mVoLTEstyle = Settings.System.getIntForUser(resolver,
+                Settings.System.VOLTE_ICON_STYLE, 0,
+                UserHandle.USER_CURRENT);
         mapIconSets();
         updateTelephony();
         notifyListeners();
@@ -488,10 +498,24 @@ public class MobileSignalController extends SignalController<
         int resId = 0;
 
         if ( mCurrentState.imsRegistered && mVoLTEicon) {
-            if (mShowHDVolte) {
-                resId = R.drawable.ic_hd_volte;
-            } else {
-                resId = R.drawable.ic_volte;
+            switch(mVoLTEstyle) {
+                // VoLTE
+                case 1:
+                    resId = R.drawable.ic_volte1;
+                    break;
+                // OOS VoLTE
+                case 2:
+                    resId = R.drawable.ic_volte2;
+                    break;
+                // HD Icon
+                case 3:
+                    resId = R.drawable.ic_hd_volte;
+                    break;
+ 	        //Vo
+                case 0:
+                default:
+                    resId = R.drawable.ic_volte;
+                    break;
             }
         }
         return resId;
