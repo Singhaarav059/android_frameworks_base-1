3f4ef5ea111225cbee5673c776b801d23221b115
00day0 <therandomuser11@gmail.com>
2019-12-26 22:16:06 +0100

    OxygenOS styled dismiss all button [1/2]
    showing animation might glitch if previous hiding animation was canceled before it finished and set visibility of button to View.GONE
    thats the reason why hiding animation is so short
    
    Before someone will run saying its kang of @imjyotiraditya solution
    I actually didn't know it was already done by him so i created my own solution
    we both looked at oxygenos code to see how it was made so thats why our code is simillar
    
    Signed-off-by: 00day0 <therandomuser11@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6372,6 +6372,12 @@ public final class Settings {
         public static final String QS_SMART_PULLDOWN = "qs_smart_pulldown";
 
         /**
+         * DismissAll Button
+         * @hide
+         */
+        public static final String DISMISS_ALL_BUTTON = "dismiss_all_button";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
