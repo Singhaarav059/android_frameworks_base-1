a3813a9445b2645ac60dc3fd3fef9384d3b0ef06
00day0 <therandomuser11@gmail.com>
2020-01-21 17:07:42 +0100

    QS Header data usage info [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6421,6 +6421,12 @@ public final class Settings {
 
 
         /**
+         * Show data usage in QS header
+         * @hide
+         */
+        public static final String QS_DATAUSAGE = "qs_datausage";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
