0edaf958089167abf14eb9a9205280f020aa4dc0
Ayush-Walia <ayushwalia999@gmail.com>
2020-03-28 04:31:18 +0000

    Revert "NetworkTraffic: Update for Android 10"
    This reverts commit e0071a3853abeebf31af5df46ef3c31d9135bbca.

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTraffic.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTraffic.java
@@ -36,9 +36,7 @@ import com.android.internal.util.havoc.CutoutUtils;
 import com.android.systemui.Dependency;
 import com.android.systemui.R;
 import com.android.systemui.statusbar.StatusIconDisplayable;
-import com.android.systemui.plugins.DarkIconDispatcher;
-import com.android.systemui.plugins.DarkIconDispatcher.DarkReceiver;
-
+import com.android.systemui.statusbar.policy.DarkIconDispatcher.DarkReceiver;
 /*
 *
 * Seeing how an Integer object in java requires at least 16 Bytes, it seemed awfully wasteful
@@ -386,7 +384,7 @@ public class NetworkTraffic extends TextView implements StatusIconDisplayable {
     }
 
     @Override
-    public void setVisibleState(int state, boolean animate) {
+    public void setVisibleState(int state) {
         if (state == mVisibleState) {
             return;
         }
