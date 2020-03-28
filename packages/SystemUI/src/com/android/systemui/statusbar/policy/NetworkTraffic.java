bfd6be89ace1bcc521fcc6d4ffcf20f44b956e6f
Ayush-Walia <ayushwalia999@gmail.com>
2020-03-28 04:43:21 +0000

    Revert "NetworkTraffic: Skip more useless handler calls"
    This reverts commit 6e5d6483ff9deabd512a89cb3299728791323b41.

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTraffic.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTraffic.java
@@ -58,8 +58,6 @@ public class NetworkTraffic extends TextView implements DarkReceiver {
     private int mAutoHideThreshold;
     private int mTintColor;
 
-    private boolean mScreenOn = true;
-
     private Handler mTrafficHandler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
@@ -221,15 +219,10 @@ public class NetworkTraffic extends TextView implements DarkReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
-            if (action == null) return;
-
-            if (action.equals(ConnectivityManager.CONNECTIVITY_ACTION) && mScreenOn) {
+            if (action != null && action.equals(ConnectivityManager.CONNECTIVITY_ACTION)
+                    || action.equals(Intent.ACTION_SCREEN_ON)) {
                 updateSettings();
-            } else if (action.equals(Intent.ACTION_SCREEN_ON)) {
-                mScreenOn = true;
-                updateSettings();
-            } else if (action.equals(Intent.ACTION_SCREEN_OFF)) {
-                mScreenOn = false;
+            } else if (action != null && action.equals(Intent.ACTION_SCREEN_OFF)) {
                 clearHandlerCallbacks();
             }
         }
